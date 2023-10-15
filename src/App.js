711df7952309bb015547885b5c0ece57b79d9e61
BilalAhmed_358 <ahmedkhanbilal358@gmail.com>
2023-10-15 21:43:27 +0500

    Movies thumbnail and titles loading

--- src/App.js
+++ src/App.js
@@ -1,9 +1,51 @@
 import './App.css';
-import api from './api/axiosConfig'
+import api from './api/axiosConfig';
+import { useState, useEffect } from 'react';
+import { Layout } from './components/Layout';
+import { Routes, Route } from 'react-router-dom';
+import Home from './components/home/Home';
+import { Grid } from '@mui/material';
+import { Row } from 'react-bootstrap';
 function App() {
+  const [movies, setMovies] = useState();
+
+  const getMovies = async () => {
+
+    try {
+      const response = await api.get('/api/v1/movies');
+      console.log(response.data)
+      setMovies(response.data);
+
+    }
+    catch (error) {
+      console.log(error);
+
+    }
+
+
+  }
+
+  useEffect(() => {
+    getMovies();
+  }, [])
+
+
+  if (!movies) {
+    return (
+      <>
+        <Grid>
+          <Row className="text-center"><h1>Loading....</h1></Row>
+        </Grid>
+      </>)
+  }
+
   return (
     <div className="App">
-
+      <Routes>
+        <Route path='/' element={<Layout />}>
+          <Route path='/' element={<Home movies={movies} />}></Route>
+        </Route>
+      </Routes>
     </div>
   );
 }
---
+++ src/components/home/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Hero from '../hero/Hero'
+
+const Home = ({ movies }) => {
+    // if (movies !== undefined) {
+
+    return (
+        <Hero movies={movies} />
+
+    )
+
+}
+
+export default Home
